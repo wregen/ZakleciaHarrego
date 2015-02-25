c15d391ec2bd5ce2e5c03bc88c5af3798a9cc476
wregen <W.Regenczuk@yahoo.com>
2015-02-25 05:11:04 +0100

    Menu added

--- src/org/wregen/spells/SpellActivity.java
+++ src/org/wregen/spells/SpellActivity.java
@@ -7,6 +7,8 @@ import org.wregen.spells.entity.Spell;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -53,6 +55,26 @@ public class SpellActivity extends ActionBarActivity {
     @Override
     public void onResume() {
         super.onResume();
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
